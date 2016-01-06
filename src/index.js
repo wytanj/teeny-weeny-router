d09bcd57e44277762c1d3e5a5049a0c7cc81fc7d
Albert <albertlimca@gmail.com>
2016-01-06 16:39:39 +0800

    Update router to execute view function

--- src/index.js
+++ src/index.js
@@ -18,16 +18,10 @@ export default class TeenyWeenyRouter {
   }
 
   route () {
-    Array.from(document.querySelectorAll('section'))
-      .forEach(section => section.style.display = 'none')
+    const router = this.routes
+      .sort((a, b) => b.route.source.length - a.route.source.length)
+      .find(obj => obj.route.test(window.location.pathname))
 
-    const router = this.routes.sort((a, b) => {
-      return b.router.source.length - a.router.source.length
-    }).find(obj => {
-      return obj.router.test(window.location.pathname)
-    })
-
-    document.title = router.title
-    document.querySelector(router.element).style.display = 'block'
+    if (router) router.view()
   }
 }
