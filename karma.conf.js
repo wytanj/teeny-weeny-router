83f52a3251f5722c60a704d91df7cbace450fb9c
Albert <albertlimca@gmail.com>
2016-01-07 19:40:21 +0800

    Use istanbul for coverage report

--- karma.conf.js
+++ karma.conf.js
@@ -57,16 +57,23 @@ module.exports = function (config) {
     browserify: {
       debug: true,
       transform: [
-        ['babelify']
-      //   ['stylify', { use: 'nib' }],
-      //   ['jadeify', { compileDebug: true, pretty: true }]
+        ['babelify'],
+        ['browserify-istanbul', { instrumenterConfig: { embedSource: true } }]
       ]
     },
 
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: process.env.CI ? ['dots', 'saucelabs'] : ['progress'],
+    reporters: process.env.CI ? ['dots', 'saucelabs', 'coverage'] : ['progress', 'coverage'],
+
+    coverageReporter: {
+      reporters: [
+        {type: 'html', dir: 'coverage'},
+        {type: 'lcov'},
+        {type: 'text'}
+      ]
+    },
 
     // web server port
     port: 9876,
