87091c917755f5a6c4496f4cb1ba3ce43d5f3a2d
Albert <albertlimca@gmail.com>
2016-01-07 20:03:12 +0800

    remove unnecessary ternary

--- karma.conf.js
+++ karma.conf.js
@@ -94,7 +94,7 @@ module.exports = function (config) {
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
-    singleRun: process.env.CI ? true : false,
+    singleRun: process.env.CI,
 
     // Concurrency level
     // how many browser should be started simultaneous
