cf1a5477cb840af025a3f8a50f84ac82bcc1c5d7
Albert <albertlimca@gmail.com>
2016-01-07 17:01:47 +0800

    fix standard

--- karma.conf.js
+++ karma.conf.js
@@ -1,7 +1,7 @@
 // Karma configuration
 // Generated on Thu Jan 07 2016 10:36:13 GMT+0800 (SGT)
 
-module.exports = function(config) {
+module.exports = function (config) {
   const customLaunchers = {
     sl_chrome: {
       base: 'SauceLabs',
@@ -33,11 +33,9 @@ module.exports = function(config) {
     // base path that will be used to resolve all patterns (eg. files, exclude)
     basePath: '',
 
-
     // frameworks to use
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
     frameworks: ['browserify', 'mocha'],
-
 
     // list of files / patterns to load in the browser
     files: [
@@ -45,11 +43,9 @@ module.exports = function(config) {
       'test/**/*.js'
     ],
 
-
     // list of files to exclude
     exclude: [
     ],
-
 
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
@@ -72,28 +68,22 @@ module.exports = function(config) {
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
     reporters: process.env.CI ? ['dots', 'saucelabs'] : ['progress'],
 
-
     // web server port
     port: 9876,
 
-
     // enable / disable colors in the output (reporters and logs)
     colors: true,
-
 
     // level of logging
     // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
     logLevel: config.LOG_INFO,
 
-
     // enable / disable watching file and executing tests whenever any file changes
     autoWatch: true,
-
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
     browsers: process.env.CI ? Object.keys(customLaunchers) : ['Chrome'],
-
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
